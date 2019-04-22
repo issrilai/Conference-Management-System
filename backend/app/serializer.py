e0c854c58d5ee06f805d20e26410df46890b20ca
rzvpop <razvan.pop1998@uahoo.com>
2019-04-22 18:37:02 +0300

    serializer fixed

--- backend/app/serializer.py
+++ backend/app/serializer.py
@@ -1,11 +1,7 @@
 from rest_framework import serializers
-<<<<<<< HEAD
-from django.contrib.auth.models import User
-=======
 # from django.contrib.auth.models import User
 from app.models import Conference
 from app.models.user import User
->>>>>>> origin/master
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,13 +9,10 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username')
 
-<<<<<<< HEAD
-=======
 
 class ConferenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conference
         fields = ('id',
-            'name', 'dateStart', 'dateStop', 'abstractDeadline', 'submitDeadline', 'bidDeadline', 'reviewDeadline')
-
->>>>>>> origin/master
+                  'name', 'dateStart', 'dateStop', 'abstractDeadline', 'submitDeadline', 'bidDeadline',
+                  'reviewDeadline')
