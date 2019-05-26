47d508459e81801d04bd2897647aa205db923ddc
Andreea <andreeaanbrus@yahoo.com>
2019-05-26 21:18:56 +0300

    ui for add conference with committee members

--- backend/app/views/committe_member_view.py
+++ backend/app/views/committe_member_view.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 
 from app.models import ProgramCommitteeMember
-from app.serializer import ProgramCommitteeAndInfoSerializer
+from app.serializer import   ProgramCommitteeAndInfoSerializer
 
 
 class CommitteeMembersView(viewsets.ModelViewSet):
